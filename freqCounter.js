65e7675052e616f23bb8a4fe85089c483868c98a
Captaintesla1998 <53061372+Captaintesla1998@users.noreply.github.com>
2020-02-04 23:07:50 +0530

    Update freqCounter.js

--- freqCounter.js
+++ freqCounter.js
@@ -14,3 +14,31 @@ function sameCheck(arr1,arr2){
 }
 console.log(sameCheck([1,2,6,7,7],[49,49,1,4,36]))
 // ----------------------------------------------------------
+// O(n^2)
+
+// ----------------------------------------------------------
+function sameCheck2(arr1,arr2){
+    if (arr1.length !== arr2.length){
+       return false;
+   }
+   
+   var frequencycounter1= {};
+   var frequencycounter2= {};
+   for(let val in arr1){
+      frequencycounter1[val] = (frequencycounter1[val] || 0) + 1
+   }
+   for(let val in arr2){
+      frequencycounter2[val] = (frequencycounter2[val] || 0) + 1
+   }
+   console.log(frequencycounter1);
+   console.log(frequencycounter2);
+   for(let key in arr1){
+      if(!(key**2) in frequencycounter2){
+         return false
+      }
+      if(frequencycounter2[key**2] !== frequencycounter1[key]){
+         return false
+      }
+   }
+   return true
+}
